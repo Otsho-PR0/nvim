056d51cbcc2f99ad5455a3f358fbbd8ebe9c5e11
Otsho-PR0 <otsho-pr0@proton.me>
2026-06-04 05:09:46 +0100

    Changin json to jsonc for comments

--- lua/config/vim.lua
+++ lua/config/vim.lua
@@ -32,4 +32,4 @@ vim.g.loaded_python3_provider = 0
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_ruby_provider = 0
 
-vim.filetype.add { extension = { hlsl = "hlsl", csp = "html" } }
+vim.filetype.add { extension = { hlsl = "hlsl", csp = "html", json = "jsonc" } }
